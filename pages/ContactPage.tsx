bc78c1d712fa340031970090bd7cb07be6ab62c2
mf.farooq75 <mf.farooq75@gmail.com>
2025-09-05 07:38:05 +0000

    Updated contact information and other site enhancements

--- pages/ContactPage.tsx
+++ pages/ContactPage.tsx
@@ -75,7 +75,7 @@ const ContactPage: React.FC = () => {
               </div>
               <div className="flex items-start gap-4">
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-amber-400 flex-shrink-0 mt-1" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" /><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" /></svg>
-                <span>123 Fashion Ave, New York, NY 10018, USA</span>
+                <span>Plot 4/1, Sector 23, Korangi Industrial-Area, Karachi-Pakistan</span>
               </div>
             </div>
           </div>
